767c140c8f46386f70982c94cd56076e7415230f
ChibuezeSamObisike <samobisike@gmail.com>
2024-04-30 11:58:37 +0100

    chore: push screens

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
 import "./styles.css";
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
+import Mob, { MobCarousel } from "./views-mobile/Mob";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <div style={{ height: "100vh" }}>
+        <MobCarousel />
+      </div>
     </ThemeProvider>
   </React.StrictMode>
 );
