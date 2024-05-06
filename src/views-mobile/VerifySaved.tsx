84fd1a213fb66a642fd39d3f62c4162b14e8eaa0
ChibuezeSamObisike <samobisike@gmail.com>
2024-05-06 18:19:17 +0100

    chore: remove swipeable drawer

--- src/views-mobile/VerifySaved.tsx
+++ src/views-mobile/VerifySaved.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, SwipeableDrawer, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import SwipeableEdgeDrawer from "../components/SwipeableDrawer";
 
 const VerifySaved = () => {
