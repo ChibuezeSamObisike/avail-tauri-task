2471b6e90c9d3dde8ef806c432597224501cc671
ChibuezeSamObisike <samobisike@gmail.com>
2024-05-07 10:18:33 +0100

    chore: upload secret recovery drawer

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,19 @@ import ReactDOM from "react-dom/client";
 import "./styles.css";
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
+// import SecretRecovery from "./views-mobile/SecretRecovery";
+import VerifySaved from "./views-mobile/VerifySaved";
+import BackupWallet from "./views-mobile/BackupWallet";
+import SecretRecovery from "./views-mobile/SecretRecovery";
 import Mob, { MobCarousel } from "./views-mobile/Mob";
+import Home from "./Home";
+import WalletChoser from "./WalletChoser";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <div style={{ height: "100vh" }}>
-        <MobCarousel />
+        <VerifySaved />
       </div>
     </ThemeProvider>
   </React.StrictMode>
